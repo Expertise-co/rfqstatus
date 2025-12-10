cb8f468abf33f773f1ea895cd3dfa0e10b77f21d
Expertise-co <kpiexpertise@gmail.com>
2025-12-10 16:03:57 +0530

    Updated final code main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ st.set_page_config(layout="wide")
 modern_style = """
 <style>
 #MainMenu {visibility: hidden;}
-
+header {visibility: hidden;}
 footer {visibility: hidden;}
 :root { color-scheme: light !important; }
 html, body, [data-testid="stAppViewContainer"], [data-testid="stSidebar"] {
@@ -168,3 +168,4 @@ if not filtered_df.empty:
 
 else:
     st.warning("⚠️ No data found for the selected filters.")
+
